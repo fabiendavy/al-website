1c6ac6b0bf68cae6da4394eaab926859b0b8b8ca
Fabien Davy <davyfabien@gmail.com>
2020-03-10 17:02:30 +0100

    integrated action text

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :reviews, dependent: :destroy
   has_many :tags, through: :article_tags
-
+  has_rich_text :content
+  
   validates :title, presence: true
-  validates :content, presence: true
+  validates :image_url, presence: true
 end
---
+++ db/migrate/20200310153048_remove_content_to_articles.rb
@@ -0,0 +1,5 @@
+class RemoveContentToArticles < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :articles, :content
+  end
+end
