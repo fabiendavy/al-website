25d841d7d32c979b0918ac69fc9128a68697e182
Fabien Davy <davyfabien@gmail.com>
2020-03-10 18:22:56 +0100

    implementing the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,21 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.author = current_user
+    @article.reading_time = ((@article.content.body.to_plain_text.split(' ').count) / 200.0).round
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :image_url, :content)
+  end
 end
