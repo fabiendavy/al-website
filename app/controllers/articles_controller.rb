3178fd603f7bc600c89ff8b924643dc094507192
Fabien Davy <davyfabien@gmail.com>
2020-03-12 17:28:23 +0100

    made the show and navbar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,13 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
   def index
-    @articles = Article.all    
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @initials = set_initials(@article.author.username)
   end
 
   def new
@@ -12,7 +19,7 @@ class ArticlesController < ApplicationController
     @article.author = current_user
     @article.reading_time = ((@article.content.body.to_plain_text.split(' ').count) / 200.0).round
     if @article.save
-      redirect_to articles_path
+      redirect_to article_path(@article)
     else
       render :new
     end
@@ -22,5 +29,11 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :image_url, :content)
+  end
+
+  def set_initials(username)
+    words = username.split(/\W/)
+    initials = words.map { |word| word[0] }
+    initials.join('')
   end
 end
